a95c8f9c9bc14bbc1cb0bedeb2acb7cdb76da352
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-01-25 18:40:55 +0300

    Completed Lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>
+      About Me
+    </h2>
+    <p>
+      Im having fun now
+    </p>
+    <img src={image} alt="I made this" srcset=""></img>
+  </div>;
 }
 
 export default About;
